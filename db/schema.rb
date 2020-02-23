b1ae1dce94508a7a3ef1d9d33776698692fad20b
Mithesh <mithesh109@gmail.com>
2020-02-23 22:11:22 +0530

    Add PhotoShouts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200223160008) do
+ActiveRecord::Schema.define(version: 20200223162848) do
+
+  create_table "photo_shouts", force: :cascade do |t|
+    t.string   "image_file_name",    null: false
+    t.string   "image_content_type", null: false
+    t.bigint   "image_file_size",    null: false
+    t.datetime "image_updated_at",   null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
 
   create_table "shouts", force: :cascade do |t|
     t.integer  "user_id",      null: false
